29615e187ab07562d490987e4e94fb81f621420b
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-14 01:20:05 +0530

    Food and heart interactions made, via adding class DragImage

---
+++ Little Monster/DragImage.swift
@@ -0,0 +1,33 @@
+//
+//  DragImage.swift
+//  Little Monster
+//
+//  Created by mitesh soni on 13/06/16.
+//  Copyright © 2016 Mitesh Soni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class DragImage: UIImageView {
+    
+    var originalPosition : CGPoint!;
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        originalPosition = self.center;
+    }
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first{
+            let position = touch.locationInView(self.superview);
+            self.center = CGPointMake(position.x, position.y);
+        }
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.center = originalPosition;
+    }
+}
