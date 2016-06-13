2d6486dc8da89ec2aa61f0e3df8955d9b4f8d14d
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-13 14:51:43 +0530

    Worked on UI and monster Idle animation

--- Little Monster/ViewController.swift
+++ Little Monster/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var monsterImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var animationImageArray = [UIImage]();
+        for (var i = 1 ; i <= 4 ; i += 1){
+            var image = (UIImage(named: "idle\(i).png"));
+            animationImageArray.append(image!);
+        }
+        
+        monsterImageView.animationImages = animationImageArray;
+        monsterImageView.animationRepeatCount = 0;
+        monsterImageView.animationDuration = 0.8;
+        monsterImageView.startAnimating();
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
